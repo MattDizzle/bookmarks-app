fcd43bc410a8708143c25afe8c63cd4f7d6554f2
Matthew Rougely <matthewrougelysr@gmail.com>
2020-07-07 18:56:48 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$SqEBgMvTmagEcE4whUdwv.33xXkdlSc0H9wPMBJaYZf8J3L2mzcle',
+  API_KEY: process.env.REACT_APP_API_KEY,
 }
